ccad4e68d165152a7ebff8386a13acbbfa65d168
remicorne <remi.corne@gmail.com>
2020-02-08 11:26:31 +0100

    tri shake

--- ProjetTris.c
+++ ProjetTris.c
@@ -5,8 +5,10 @@
 void Tri_Insertion(int t[], int lg) // possibilité amélioration avec recherche dicho sur la partie triée
 {
     int k = 1, j, temp;
+    int count = 0;
     while (k < lg)
     {
+        printf("%d", count++);
         j = k;
         temp = t[k];
         while (j > 0 && temp < t[j - 1])
@@ -22,8 +24,10 @@ void Tri_Insertion(int t[], int lg) // possibilité amélioration avec recherche
 void Tri_Insertion_Dichotomie(int t[], int lg) 
 {
     int k = 1, j, i, temp;
+    int count = 0;
     while (k < lg)
     {
+        printf("%d", count++);
         j = k;
         temp = t[k];
         i = 0;
@@ -51,8 +55,10 @@ void Tri_Insertion_Dichotomie(int t[], int lg)
 void Tri_Selection(int t[], int lg)
 {
     int k = lg - 1, i, j, temp;
+    int count = 0;
     while (k > 0)
     {
+        printf("%d", count++);
         j = k;
         i = k - 1;
         while (i >= 0)
@@ -73,8 +79,10 @@ void Tri_Selection(int t[], int lg)
 void Tri_Bulles(int t[], int lg)
 {
     int k = lg - 1, kp, i, temp;
+    int count = 0;
     while (k > 0)
     {
+        printf("%d", count++);
         i = 0;
         kp = 0;
         while (i < k)
@@ -92,6 +100,45 @@ void Tri_Bulles(int t[], int lg)
     }
 }
 
+void Tri_Shake(int t[], int lg)
+{
+    int kmax = lg - 1, kmin = 0, kp, i, temp;
+    int count = 0;
+    while (kmin != kmax)
+    {
+        printf("%d", count++);
+        i = kmin;
+        kp = kmin;
+        while (i < kmax)
+        {
+            if (t[i] > t[i + 1])
+            {
+                temp = t[i];
+                t[i] = t[i + 1];
+                t[i + 1] = temp;
+                kp = i + 1;
+            }
+            i++;
+        }
+        kmax = kp;
+
+        i = kmax;
+        kp = kmax;
+        while (i > kmin)
+        {
+            if (t[i] < t[i - 1])
+            {
+                temp = t[i];
+                t[i] = t[i - 1];
+                t[i - 1] = temp;
+                kp = i - 1;
+            }
+            i--;
+        }
+        kmin = kp;
+    }
+}
+
 int main()
 {
     int t20[500], i;
@@ -106,7 +153,7 @@ int main()
     }
 
     printf("\n");
-    Tri_Insertion_Dichotomie(t20, 20);
+    Tri_Shake(t20, 20);
 
     i = 0;
     while (i < 20)
